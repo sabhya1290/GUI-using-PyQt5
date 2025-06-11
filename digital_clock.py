6dd0f2bc1b5553aab4aa1470a581a376013d0948
sabhya1290 <sabhya1290@gmail.com>
2025-06-11 15:53:09 +0530

    minor_update

--- digital_clock.py
+++ digital_clock.py
@@ -23,7 +23,7 @@ class DigitalClock(QWidget):
                                       "color:hsl(99, 98%, 50%);"
                                       )
         
-        font_id= QFontDatabase.addApplicationFont("gui/DS-DIGIB.TTF")
+        font_id= QFontDatabase.addApplicationFont("DS-DIGIB.TTF")
         font_family=QFontDatabase.applicationFontFamilies(font_id)[0]
         my_font=QFont(font_family,100)
         self.time_label.setFont(my_font)
--- stop_watch.py
+++ stop_watch.py
@@ -66,7 +66,7 @@ class StopWatch(QWidget):
                                       "border-radius:20px;"
                                       )
         
-        font_id= QFontDatabase.addApplicationFont("gui/DS-DIGIB.TTF")
+        font_id= QFontDatabase.addApplicationFont("DS-DIGIB.TTF")
         font_family=QFontDatabase.applicationFontFamilies(font_id)[0]
         my_font=QFont(font_family,100)
         self.time_label.setFont(my_font)
